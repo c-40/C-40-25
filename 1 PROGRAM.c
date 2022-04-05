cb0062bc1759d5983cafe99a5fda4dde8dbedcb7
c-40 <103025887+c-40@users.noreply.github.com>
2022-04-05 00:31:21 -0700

    Add files via upload

---
+++ 1 PROGRAM.c
@@ -0,0 +1,27 @@
+Input-
+//MY FIRST C PROGRAM
+ 
+#include<stdio.h>
+
+int main()
+
+{
+    
+printf("Talk To a Teacher\n");
+    
+printf("Welcome\n");
+   
+ return 0;
+   
+ 
+}
+
+Output-
+
+Talk To a Teacher
+Welcome
+
+
+...Program finished with exit code 0
+Press ENTER to exit console.
+
---
+++ 2 Program.c
@@ -0,0 +1,38 @@
+INPUT-
+//My scond program 
+
+#include<stdio.h>
+
+int main()
+
+{
+    
+int a=2;
+   
+double const b=4;
+    
+float c=1.5;
+    
+char d='A';
+    
+printf("The value of a is %d\n",a);
+    
+printf("The vlaue of b is %lf\n",b);
+   
+printf("The value of c is %3f\n",c);
+    
+printf("The value of d is %c\n",d);
+    
+return 0;
+
+}
+
+OUTPUT-
+
+The value of a is 2
+The vlaue of b is 4.000000
+The value of c is 1.500000
+The value of d is A
+
+
+...Program finished with exit code 0
