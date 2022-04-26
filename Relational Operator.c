548a1483d45f85849f9355c2d56d4129e1827365
c-40 <103025887+c-40@users.noreply.github.com>
2022-04-26 11:48:47 +0530

    Add files via upload

---
+++ Relational Operator.c
@@ -0,0 +1,30 @@
+input-
+#include <stdio.h>
+
+int
+main ()
+{
+  int a, b;
+  printf ("Enter the value of a and b\n");
+  scanf ("%d%d", &a, &b);
+  if (a > b)
+  printf ("%d is greater than %d\n",a, b);
+  else if (a < b)
+  printf ("%d less than %d\n",a, b);
+  if (a <= b)
+    printf ("%d is less or equal to %d\n",a, b);
+  else if (a >= b)
+    printf ("%d is greater than or equal to %d\n",a, b);
+  if (a == b)
+  printf("%d is equal to %d\n",a,b);
+  else if(a=!b)
+  printf("%d is  not equal to %d\n",a,b);
+  return 0;
+}
+OUTPUT-
+8 is greater than 3
+8 is greater than or equal to 3
+
+
+...Program finished with exit code 0
+Press ENTER to exit console.
